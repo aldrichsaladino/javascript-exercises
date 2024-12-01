696d6679ffe0dcebd1a5ffd6de91f73bfb41176b
aldrichsaladino <aldrich.saladino@gmail.com>
2024-11-30 19:27:57 -0700

    Update calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
 const sum = function() {
 	
 };
 
-const multiply = function() {
-
+const multiply = function(a,b) {
+	return a*b;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.Pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	n=1
+	for( i=n;
 };
 
 // Do not edit below this line
